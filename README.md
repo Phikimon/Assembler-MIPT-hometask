adafa47acada41014fbb777604a558cf7b912ec0
phikimon <feenix550@mail.ru>
2017-03-16 01:40:05 +0300

    PrintBinHexOct and SumUntilZero finished

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 Assembler x86/x64 hometask for MIPT
 
+;   Typical usage example:
+;   .../Assembler-MIPT-hometask$ mkdir ./executables<Enter>
+;   .../Assembler-MIPT-hometask$ make PrintBinHexOct<Enter>
+;   .../Assembler-MIPT-hometask$ cd ./executables<Enter>
+;   .../Assembler-MIPT-hometask/executables$ ./PrintBinHexOct<Enter>
+;   2<C-d>            //Firstly you have to enter mode: 0 - bin, 1 - hex, 2 - dec
+;   1<C-d>            //Secondly you have to enter number to print: '1' is 0x31 and 0d49
+;   49
+;   .../Assembler-MIPT-hometask/executables$
--- src/PrintBinHexOct.asm
+++ src/PrintBinHexOct.asm
@@ -1,5 +1,15 @@
+;   Typical usage example:
+;   .../Assembler-MIPT-hometask$ mkdir ./executables<Enter>
+;   .../Assembler-MIPT-hometask$ make PrintBinHexOct<Enter>
+;   .../Assembler-MIPT-hometask$ cd ./executables<Enter>
+;   .../Assembler-MIPT-hometask/executables$ ./PrintBinHexOct<Enter>
+;   2<C-d>            //Firstly you have to enter mode: 0 - bin, 1 - hex, 2 - dec
+;   1<C-d>            //Secondly you have to enter number to print: '1' is 0x31 and 0d49
+;   49
+;   .../Assembler-MIPT-hometask/executables$
+
 ;define constant
-            NUMSTRINGLEN equ 64
+        NUMSTRINGLEN equ 64
 ;macros
 %macro exit 1
     ;Exit from program                                            
