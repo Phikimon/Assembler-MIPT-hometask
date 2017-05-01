aa363efb036c178e84b8a894b26e9266fd3bce4a
phikimon <feenix550@mail.ru>
2017-05-01 18:03:59 +0300

    Repair main.c and README.md

--- printflib/main.c
+++ printflib/main.c
@@ -2,7 +2,8 @@ void PhilPrintf(char* formatString, ...);
 
 int main(void)
 {
-    PhilPrintf("I love %s very much and %s loves me too %d(%x(%o(%b))) times\n",
-               "scanf", "printf", 100, 100, 100, 100);
+    PhilPrintf("I love %s very much and %s loves me too %d(%x(%o(%b))) times, %c, %s"
+               ", %x, %d, %%\n",
+               "EDA", "printf", 100, 100, 100, 100, 'I', "LOVE", 3802, 100);
     return 0;
 }
